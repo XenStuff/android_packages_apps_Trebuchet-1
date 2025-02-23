TheDoctor12/android_packages_apps_Launcher3

Launcher3: Add un-themed icons to google search bar
Themed icons should only be used when themed icons are
enabled by user in ThemePicker. Otherwise, use the
standard google colors. Matches Pixel Launcher.

Change-Id: If6e81f746f2ab7875da81fd24b3663fad03e5fe9
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: Patryk Michalik <contact@patrykmichalik.com>
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

src/com/android/launcher3/qsb/QsbLayout.java
@@ -1,27 +1,32 @@
 package com.android.launcher3.qsb;
 
+import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import androidx.core.view.ViewCompat;
 import com.android.launcher3.BaseActivity;
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.qsb.QsbContainerView;
+import com.android.launcher3.util.Themes;
 import com.android.launcher3.views.ActivityContext;
-import android.view.View;
 
-public class QsbLayout extends FrameLayout {
+public class QsbLayout extends FrameLayout implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
-    ImageButton lensIcon;
-    AssistantIconView assistantIcon;
+    ImageView mAssistantIcon;
+    ImageView mGoogleIcon;
+    ImageView mLensIcon;
     Context mContext;
 
     public QsbLayout(Context context, AttributeSet attrs) {
@@ -33,18 +38,25 @@ public class QsbLayout extends FrameLayout {
         super(context, attrs, defStyle);
         mContext = context;
     }
+
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        assistantIcon = findViewById(R.id.mic_icon);
-        assistantIcon.setIcon();
+        mAssistantIcon = findViewById(R.id.mic_icon);
+        mGoogleIcon = findViewById(R.id.g_icon);
+        mLensIcon = findViewById(R.id.lens_icon);
+        setIcons();
+
+        Utilities.getPrefs(mContext).registerOnSharedPreferenceChangeListener(this);
+
         String searchPackage = QsbContainerView.getSearchWidgetPackageName(mContext);
         setOnClickListener(view -> {
             mContext.startActivity(new Intent("android.search.action.GLOBAL_SEARCH").addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
                 Intent.FLAG_ACTIVITY_CLEAR_TASK).setPackage(searchPackage));
         });
+
         if (Utilities.isGSAEnabled(mContext)) {
-            setupLensIcon();
+            enableLensIcon();
         }
     }
 
@@ -67,11 +79,28 @@ public class QsbLayout extends FrameLayout {
         }
     }
 
-    private void setupLensIcon() {
-        lensIcon = findViewById(R.id.lens_icon);
-        lensIcon.setVisibility(View.VISIBLE);
-        lensIcon.setImageResource(R.drawable.ic_lens_color);
-        lensIcon.setOnClickListener(view -> {
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        if (key.equals(Themes.KEY_THEMED_ICONS)) {
+            setIcons();
+        }
+    }
+
+    private void setIcons() {
+        if (Themes.isThemedIconEnabled(mContext)) {
+            mAssistantIcon.setImageResource(R.drawable.ic_mic_themed);
+            mGoogleIcon.setImageResource(R.drawable.ic_super_g_themed);
+            mLensIcon.setImageResource(R.drawable.ic_lens_themed);
+        } else {
+            mAssistantIcon.setImageResource(R.drawable.ic_mic_color);
+            mGoogleIcon.setImageResource(R.drawable.ic_super_g_color);
+            mLensIcon.setImageResource(R.drawable.ic_lens_color);
+        }
+    }
+
+    private void enableLensIcon() {
+        mLensIcon.setVisibility(View.VISIBLE);
+        mLensIcon.setOnClickListener(view -> {
             Intent lensIntent = new Intent();
             Bundle bundle = new Bundle();
             bundle.putString("caller_package", Utilities.GSA_PACKAGE);

XenStuff/android_packages_apps_Trebuchet-1

src/com/android/launcher3/qsb/QsbLayout.java
@@ -1,27 +1,33 @@
 package com.android.launcher3.qsb;
 
+import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import androidx.core.view.ViewCompat;
 import com.android.launcher3.BaseActivity;
 import com.android.launcher3.DeviceProfile;
+import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.qsb.QsbContainerView;
+import com.android.launcher3.util.Themes;
 import com.android.launcher3.views.ActivityContext;
-import android.view.View;
 
-public class QsbLayout extends FrameLayout {
+public class QsbLayout extends FrameLayout implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
-    ImageButton lensIcon;
-    AssistantIconView assistantIcon;
+    ImageView mAssistantIcon;
+    ImageView mGoogleIcon;
+    ImageView mLensIcon;
     Context mContext;
 
     public QsbLayout(Context context, AttributeSet attrs) {
@@ -33,18 +39,25 @@ public class QsbLayout extends FrameLayout {
         super(context, attrs, defStyle);
         mContext = context;
     }
+
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        assistantIcon = findViewById(R.id.mic_icon);
-        assistantIcon.setIcon();
+        mAssistantIcon = findViewById(R.id.mic_icon);
+        mGoogleIcon = findViewById(R.id.g_icon);
+        mLensIcon = findViewById(R.id.lens_icon);
+        setIcons();
+
+        LauncherPrefs.getPrefs(mContext).registerOnSharedPreferenceChangeListener(this);
+
         String searchPackage = QsbContainerView.getSearchWidgetPackageName(mContext);
         setOnClickListener(view -> {
             mContext.startActivity(new Intent("android.search.action.GLOBAL_SEARCH").addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
                 Intent.FLAG_ACTIVITY_CLEAR_TASK).setPackage(searchPackage));
         });
+
         if (Utilities.isGSAEnabled(mContext)) {
-            setupLensIcon();
+            enableLensIcon();
         }
     }
 
@@ -67,11 +80,28 @@ public class QsbLayout extends FrameLayout {
         }
     }
 
-    private void setupLensIcon() {
-        lensIcon = findViewById(R.id.lens_icon);
-        lensIcon.setVisibility(View.VISIBLE);
-        lensIcon.setImageResource(R.drawable.ic_lens_color);
-        lensIcon.setOnClickListener(view -> {
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        if (key.equals(Themes.KEY_THEMED_ICONS)) {
+            setIcons();
+        }
+    }
+
+    private void setIcons() {
+        if (Themes.isThemedIconEnabled(mContext)) {
+            mAssistantIcon.setImageResource(R.drawable.ic_mic_themed);
+            mGoogleIcon.setImageResource(R.drawable.ic_super_g_themed);
+            mLensIcon.setImageResource(R.drawable.ic_lens_themed);
+        } else {
+            mAssistantIcon.setImageResource(R.drawable.ic_mic_color);
+            mGoogleIcon.setImageResource(R.drawable.ic_super_g_color);
+            mLensIcon.setImageResource(R.drawable.ic_lens_color);
+        }
+    }
+
+    private void enableLensIcon() {
+        mLensIcon.setVisibility(View.VISIBLE);
+        mLensIcon.setOnClickListener(view -> {
             Intent lensIntent = new Intent();
             Bundle bundle = new Bundle();
             bundle.putString("caller_package", Utilities.GSA_PACKAGE);